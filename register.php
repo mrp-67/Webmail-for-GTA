b722a5d134041cd9d25cc8ed479cd9995874797a
reality-rp <mustafaaipo2200@outlook.sa>
2021-10-16 21:59:42 +0200

    register
    register

--- register.php
+++ register.php
@@ -6,6 +6,66 @@ session_start();
 <html> 
 <head>
   <title>Registrierung</title>  
+  <style>
+      body {
+        margin: 0;
+        display: grid;
+        width: 100%;
+        height: 100%;
+      }
+
+      .form {
+        margin: auto;
+        margin-top: 22%;
+      }
+
+      .email {
+        width: 26vh;
+        height: 3vh;
+        text-align: center;
+        margin: 1vh 0;
+      }
+
+      .password {
+        width: 26vh;
+        height: 3vh;
+        text-align: center;
+        margin: 1vh 0;
+      }
+
+      .submit {
+        margin: 3vh auto;
+        display: flex;
+        font-size: 2vh;
+        padding: 1vh 6vh;
+        border: 0;
+        background: #ffc107;
+        border-radius: 5px;
+      }
+
+      div {
+        background: #ffc107;
+        height: auto;
+        width: auto;
+        margin: auto;
+        display: flex;
+        margin-top: 12vh;
+        position: fixed;
+        border-radius: 5px;
+      }
+
+      p {
+        text-align: center;
+        width: auto;
+        height: auto;
+        padding: 0.6vh 4vh;
+        color: #ff0000;
+        font-size: 20px;
+        margin: 0;
+        font-family: arial;
+      }
+
+  </style>  
 </head> 
 <body>
  
@@ -19,15 +79,15 @@ if(isset($_GET['register'])) {
     $passwort2 = $_POST['passwort2'];
   
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo '<div>Bitte eine gültige E-Mail-Adresse eingeben</div>';
+        echo '<div><p>Bitte eine gültige E-Mail-Adresse eingeben<p></div><br>';
         $error = true;
     }     
     if(strlen($passwort) == 0) {
-        echo 'Bitte ein Passwort angeben<br>';
+        echo '<div><p>Bitte ein Passwort angeben<p></div><br>';
         $error = true;
     }
     if($passwort != $passwort2) {
-        echo 'Die Passwörter müssen übereinstimmen<br>';
+        echo '<div><p>Die Passwörter müssen übereinstimmen<p></div><br>';
         $error = true;
     }
     
@@ -38,7 +98,7 @@ if(isset($_GET['register'])) {
         $user = $statement->fetch();
         
         if($user !== false) {
-            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+            echo '<div><p>Diese E-Mail-Adresse ist bereits vergeben<p></div><br>';
             $error = true;
         }    
     }
@@ -51,10 +111,10 @@ if(isset($_GET['register'])) {
         $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
         
         if($result) {        
-            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+            echo '<div><p>Du wurdest erfolgreich registriert. <a href="login.php">Zum Login<p></div><br>';
             $showFormular = false;
         } else {
-            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+            echo '<div><p>Beim Abspeichern ist leider ein Fehler aufgetreten<p></div><br>';
         }
     } 
 }

d8d83be955c2b1515309d4451ce046723a088eac
mrp-67 <mrrampage67@web.de>
2021-10-16 21:36:16 +0200

    update

--- register.php
+++ register.php
@@ -6,65 +6,6 @@ session_start();
 <html> 
 <head>
   <title>Registrierung</title>  
-  <style>
-      body {
-        margin: 0;
-        display: grid;
-        width: 100%;
-        height: 100%;
-      }
-
-      .form {
-        margin: auto;
-        margin-top: 23%;
-      }
-
-      .email {
-        width: 26vh;
-        height: 3vh;
-        text-align: center;
-        margin: 1vh 0;
-      }
-
-      .password {
-        width: 26vh;
-        height: 3vh;
-        text-align: center;
-        margin: 1vh 0;
-      }
-
-      .submit {
-        margin: 3vh auto;
-        display: flex;
-        font-size: 2vh;
-        padding: 1vh 6vh;
-        border: 0;
-        background: #ffc107;
-        border-radius: 5px;
-      }
-
-      div {
-        background: #ffc107;
-        height: auto;
-        width: auto;
-        margin: auto;
-        display: flex;
-        margin-top: 12vh;
-        position: fixed;
-        border-radius: 5px;
-      }
-
-      p {
-        text-align: center;
-        width: auto;
-        height: auto;
-        padding: 0.6vh 4vh;
-        color: #ff0000;
-        font-size: 20px;
-        margin: 0;
-        font-family: arial;
-      }
-  </style> 
 </head> 
 <body>
  

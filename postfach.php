b967e5a25733bc3e1384829ce0292f39086cee9f
reality-rp <mustafaaipo2200@outlook.sa>
2021-10-19 20:14:47 +0200

    Update postfach.php

--- postfach.php
+++ postfach.php
@@ -11,7 +11,20 @@ $email = $_SESSION['email'];
 <html> 
 <head>
   <meta charset="UTF-8">
-  <title>Postfach</title>  
+  <title>Postfach</title>
+  <style>  
+    div {
+      background: gold;
+      width: 20%;
+      height: 20vh;
+      margin: 10vh;
+    }
+
+    div:hover {
+      background: red;
+    }
+  
+  </style> 
 </head> 
 <body>
 
@@ -22,14 +35,23 @@ foreach ($pdo->query($sql) as $row){
   echo $row['absender']."<br>".$row['betreff']."<br> <br>";
 }*/
 
+//Mail overview
 $sql = "SELECT * FROM emails WHERE empfaenger = '$email'";
 foreach ($pdo->query($sql) as $row){
   $absender = $row['absender'];
+  $eid = $row['eid'];
+  
   $cmd = "SELECT * FROM users WHERE email = '$absender'";
   foreach ($pdo->query($cmd) as $inf){
-    echo $inf['vorname']." ".$inf['nachname']."<br>";
+    echo "<div><button>".$inf['vorname']." ".$inf['nachname']."<br>";
+    $absenderVorname = $inf['vorname'];
+    $absenderNachname = $inf['nachname'];
   }
-  echo $row['betreff']."<br> <br>";
+  echo $row['betreff']."<br>";
+  $absenderBetreff = $row['betreff']."</button></div>";
+}
+if(isset($_GET['eid'])) {
+  echo "Das ist die EMAil ".$eid;
 }
 
 ?>

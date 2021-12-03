667bb2de6e082eef7a3f87f836f5ba3714a56e97
mrp-67 <mrrampage67@web.de>
2021-12-03 18:40:19 +0100

    Update verify.php
    It is checked that the server is online

--- verify.php
+++ verify.php
@@ -1,33 +1,42 @@
 <?php
 
-if(isset($_SESSION['email'])){
-  $sql = "SELECT * FROM users WHERE email = '$email'";
-  $user = $pdo->query($sql)->fetch();
-  $status = $user['status'];
-    if($status == 1)
-    {
-      $showFormular = false;
-      echo '<div>Ihr Konto wurde gesperrt, bitte melden Sie sich im Support.<br>Sie werden automatisch ausgeloggt.</div><meta http-equiv="refresh" content="5; URL=logout.php">';
-    }
-    elseif(isset($_SESSION['email'])){
-        $sql = "SELECT * FROM users WHERE email = '$email'";
-        $user = $pdo->query($sql)->fetch();
-        $steamid = $user['steamid'];
-
-        $suche = array($steamid);
-        $players = strip_tags(file_get_contents('http://rp.night-v.org:30120/players.json'));
-          
-        foreach($suche as $value){
-          if(stripos($players, $value) !== false){
-          }
-            else{
-              $error = true;
-              $showFormular = false;
-              echo '<div> Sie sind nicht mit unserem Gameserver verbunden.<br> Sie werden automatisch ausgeloggt.</div><meta http-equiv="refresh" content="5; URL=logout.php">';
+$url="http://rp.night-v.org:30120/players.json'";
+$check = @fsockopen($url, 80);
+If ($check) {
+  if(isset($_SESSION['email'])){
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $user = $pdo->query($sql)->fetch();
+    $status = $user['status'];
+      if($status == 1)
+      {
+        $showFormular = false;
+        echo '<div>Ihr Konto wurde gesperrt, bitte melden Sie sich im Support.<br>Sie werden automatisch ausgeloggt.</div><meta http-equiv="refresh" content="5; URL=logout.php">';
+      }
+      elseif(isset($_SESSION['email'])){
+          $sql = "SELECT * FROM users WHERE email = '$email'";
+          $user = $pdo->query($sql)->fetch();
+          $steamid = $user['steamid'];
+  
+          $suche = array($steamid);
+          $players = strip_tags(file_get_contents('http://rp.night-v.org:30120/players.json'));
+            
+          foreach($suche as $value){
+            if(stripos($players, $value) !== false){
             }
-        } 
-    } 
+              else{
+                $error = true;
+                $showFormular = false;
+                echo '<div> Sie sind nicht mit unserem Gameserver verbunden.<br> Sie werden automatisch ausgeloggt.</div><meta http-equiv="refresh" content="5; URL=logout.php">';
+              }
+          } 
+      } 
+  }
+
+} Else {
+  $showFormular = false;
+  echo '<div>Aktuell sind unsere Server unter Wartungsarbeiten.<br>Mehr Informationen dazu findest du auf unserem Discord-Server.</div><meta http-equiv="refresh" content="5; URL=logout.php">';
 }
+
 ?>
 
 
